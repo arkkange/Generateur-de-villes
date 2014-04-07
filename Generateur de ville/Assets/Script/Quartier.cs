b1d89254f04ec502edf15bd40d09fe6c6c15a52e
Julien Leseine <leseine.julien.logiciel@gmail.com>
2014-04-07 20:06:56 +0200

    1.0.0

---
+++ Generateur de ville/Assets/Script/Quartier.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quartier : MonoBehaviour {
+
+	public class quartier {
+		
+		public Position position;
+		public Taille Taille;
+		public string Type;
+		public Rue[] Rues;
+		
+		public quartier(int _position_x,
+		                int _position_y,
+		                int _taille_X,
+		                int _taille_Y,
+		                string _type){
+			
+			position = new Position(_position_x, _position_y);
+			Taille = new Taille(_taille_X, _taille_Y);
+			Type = _type;
+			
+		}
+		
+	}
+	
+	public class Rue {
+		
+		public Position Begin;
+		public Position End;
+		
+		public Rue(Position _begin, Position _End){
+			Begin = _begin;
+			End = _End;
+		}
+		
+	}
+	
+	public class Position {
+		public int x;
+		public int y;
+		
+		Position(int _x,
+		         int _y){
+			x = _x;
+			y = _y;
+		}
+		
+	}
+	
+	public class Taille {
+		public int x;
+		public int y;
+		
+		Taille(int _x,
+		       int _y){
+			x = _x;
+			y = _y;
+		}
+		
+	}
+
+}
---
+++ Generateur de ville/Assets/Script/TownGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TownGenerator : MonoBehaviour {
+
+	#region variables
+
+	[SerializeField]
+	public static int taille = 100;
+
+	public static int[,] _Road_Table = new int[taille,taille];
+
+	#endregion
+
+	#region Start
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	#endregion
+
+	#region Update
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	#endregion
+
+
+}
+
